2eaa3a6af388c26760befceaf0d03059c451335b
Gabriel <69171339+GeavaPapi@users.noreply.github.com>
2021-08-04 11:44:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const figlet = require("figlet")
 const client = new Discord.Client()
 const checkclient = require('xportgbva').testenv()
 const details = dotenv.config()
-console.log(chalk.green(figlet.textSync('Geava\nEasy Credits', { horizontalLayout: 'full' })))
+console.log(chalk.green(figlet.textSync('Geava\Fishy Spammer', { horizontalLayout: 'full' })))
 console.log(chalk.green('\nWait few seconds to connect!'))
 
 if (details) {
